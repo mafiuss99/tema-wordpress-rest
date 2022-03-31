9773ca20e586d2ac852cb1aa025b5aab2810e483
Matheus Felipe <mf.mf98.23@gmail.com>
2022-03-30 21:58:31 -0300

    Adicionando novos campos customizaveis ao tema

---
+++ README.md
@@ -0,0 +1,9 @@
+Clone o seu projeto na pasta de temas do Wordpress
+
+- Painél Wordpress
+
+Crie uma página chamada Home, selecione o modelo Home para essa página e em seguida defina esta página como a página inicial
+
+Crie uma página chamada Contato, selecione o modelo Contato para esta página
+
+Crie uma página chamada Sobre, selecione o modelo Sobre para esta página
--- page-sobre.php
+++ page-sobre.php
@@ -7,17 +7,16 @@
 			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="grid-8">
-				<img src="img/rest-fachada.jpg" alt="Fachada do Rest">
+				<img src="<?php the_field('imagem_sobre'); ?>" alt="<?php the_field('texto_imagem_sobre') ?>">
 			</div>
 
 			<div class="grid-8">
 				<h2>História</h2>
-				<p>O incentivo ao avanço tecnológico, assim como a mobilidade dos capitais internacionais auxilia a preparação e a composição dos níveis de motivação departamental.</p>
-				<p>Gostaria de enfatizar que o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes do remanejamento dos quadros funcionais.</p>
+				<?php the_field('historia'); ?>
 				<h2>Visão</h2>
-				<p>Não obstante, a expansão dos mercados mundiais faz parte de um processo de gerenciamento de alternativas às soluções ortodoxas.</p>
-				<h2>Valores</h2>
-				<p>O empenho em analisar a consolidação das estruturas apresenta tendências no sentido de aprovar a manutenção dos índices pretendidos.</p>
+				<?php the_field('visao'); ?>
+				<h2>Valores</h2>	
+				<?php the_field('valores'); ?>
 			</div>
 		</section>
 	<?php endwhile; endif;?>
